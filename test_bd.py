3964600601edb995a167f9dbecde7c3f8cbf6d01
victorvhs <victor.h.s.reis@gmail.com>
2016-09-10 14:07:36 -0300

    Gravando no data Base

--- test_bd.py
+++ test_bd.py
@@ -6,6 +6,7 @@ print( dataBase.buscarTabela("user") )
 print( dataBase.buscarTabela("ativos") )
 print( dataBase.buscarTabela("privilegios") )
 print(dataBase.buscarTabela("tipos_ativos"))
-campos=["nome,tipo,local,posicao,data"]
-dataBase.gravar("ativo",campos,["coelho",1,"pais das maravilhas","ok","12/09/1945"])
+campos=["idtipo_ativos,nome"]
+valores=["","Ebook"]
+dataBase.gravar("tipos_ativos","idtipo_ativos,nome","","Ebook")
 print( dataBase.buscarTabela("ativos") )
